c1cae45d8c4b279adafcd1404b8339b5e80c33d8
otis-ke <vincentotiskisia.com>
2023-03-09 11:08:13 -0800

    hush

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This module imports functions from calculator_1.py,
+performs some calculations, and prints the results
+"""
+
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+result1 = add(a, b)
+result2 = sub(a, b)
+result3 = mul(a, b)
+result4 = div(a, b)
+
+print("{} + {} = {}".format(a, b, result1))
+print("{} - {} = {}".format(a, b, result2))
+print("{} * {} = {}".format(a, b, result3))
+print("{} / {} = {}".format(a, b, result4))
